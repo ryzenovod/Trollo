669d2c77261b4119fcc163d11a2a93f4d1f61274
Егор Белоусов <belousov.eal@students.dvfu.ru>
2023-05-17 15:53:13 +1000

    добавлены работающие сохранения

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 import sqlite3
+
+# Create a database or connect to one
+conn = sqlite3.connect('mylist.db')
+# Create a cursor
+c = conn.cursor()
+
+# Create a table
+c.execute("""CREATE TABLE if not exists todo_list(
+    list_item text)
+    """)
+
+# Commit the changes
+conn.commit()
+
+# Close our connection
+conn.close()
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(524, 357)
+        MainWindow.resize(521, 365)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.additem_pushButton = QtWidgets.QPushButton(self.centralwidget, clicked=lambda: self.add_it())
@@ -28,7 +45,7 @@ class Ui_MainWindow(object):
         self.savedb_pushButton.setObjectName("savedb_pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 524, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 521, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -37,6 +54,29 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        # Grab all the items from the database
+        self.grab_all()
+
+    # Grab items from database
+    def grab_all(self):
+        # Create a database or connect to one
+        conn = sqlite3.connect('mylist.db')
+        # Create a cursor
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM todo_list")
+        records = c.fetchall()
+
+        # Commit the changes
+        conn.commit()
+
+        # Close our connection
+        conn.close()
+
+        # Loop thru records and add to screen
+        for record in records:
+            self.mylist_listWidget.addItem(str(record[0]))
 
     # Add Item To List
     def add_it(self):
@@ -57,45 +97,46 @@ class Ui_MainWindow(object):
         # Delete selected row
         self.mylist_listWidget.takeItem(clicked)
 
-    # Save To Database
+    # Clear All Items From List
+    def clear_it(self):
+        self.mylist_listWidget.clear()
+
+    # Save To The Database
     def save_it(self):
+        # Create a database or connect to one
+        conn = sqlite3.connect('mylist.db')
+        # Create a cursor
+        c = conn.cursor()
+
+        # Delete everything in the database table
+        c.execute('DELETE FROM todo_list;', )
+
+        # Create Blank List To Hold Items
         items = []
+        # Loop through the listWidget and pull out each item
         for index in range(self.mylist_listWidget.count()):
             items.append(self.mylist_listWidget.item(index))
 
-        # Create a database or connect to one that exists
-        conn = sqlite3.connect('my_todo_list.db')
+        for item in items:
+            # print(item.text())
+            # Add stuff to the table
+            c.execute("INSERT INTO todo_list VALUES (:item)",
+                      {
+                          'item': item.text(),
+                      })
 
-        # Create a cursor instance
-        c = conn.cursor()
-
-        c.execute("""UPDATE list SET
-            item = :item,
-
-
-            WHERE oid = :oid""",
-                  {
-                      'item': fn_entry.get(),
-                      'last': ln_entry.get(),
-                      'address': address_entry.get(),
-                      'city': city_entry.get(),
-                      'state': state_entry.get(),
-                      'zipcode': zipcode_entry.get(),
-                      'oid': id_entry.get(),
-                  })
-
-        # Commit changes
+        # Commit the changes
         conn.commit()
 
         # Close our connection
         conn.close()
 
-        for thing in items:
-            print(thing.text())
-
-    # Clear All Items From List
-    def clear_it(self):
-        self.mylist_listWidget.clear()
+        # Pop up box
+        msg = QMessageBox()
+        msg.setWindowTitle("Saved To Database!!")
+        msg.setText("Your Todo List Has Been Saved!")
+        msg.setIcon(QMessageBox.Information)
+        x = msg.exec_()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -104,25 +145,6 @@ class Ui_MainWindow(object):
         self.deleteitem_pushButton_2.setText(_translate("MainWindow", "Delete Item From List"))
         self.clearall_pushButton_3.setText(_translate("MainWindow", "Clear The List"))
         self.savedb_pushButton.setText(_translate("MainWindow", "Save To Database"))
-
-    def thing():
-        # Create a database or connect to one that exists
-        conn = sqlite3.connect('my_todo_list.db')
-
-        # Create a cursor instance
-        c = conn.cursor()
-
-        # Create Table
-        c.execute("""CREATE TABLE if not exists todo_list (
-            item text)
-            """)
-        # Commit changes
-        conn.commit()
-
-        # Close our connection
-        conn.close()
-
-    thing()
 
 
 if __name__ == "__main__":

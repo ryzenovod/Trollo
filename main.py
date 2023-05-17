4dd81188d7fda78a35b3db81ce164123715fb214
Егор Белоусов <belousov.eal@students.dvfu.ru>
2023-05-17 15:51:27 +1000

    добавлена дб

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+import sqlite3
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(405, 357)
+        MainWindow.resize(524, 357)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.additem_pushButton = QtWidgets.QPushButton(self.centralwidget, clicked= lambda: self.add_it())
+        self.additem_pushButton = QtWidgets.QPushButton(self.centralwidget, clicked=lambda: self.add_it())
         self.additem_pushButton.setGeometry(QtCore.QRect(10, 50, 121, 31))
         self.additem_pushButton.setObjectName("additem_pushButton")
-        self.deleteitem_pushButton_2 = QtWidgets.QPushButton(self.centralwidget, clicked= lambda: self.delete_it())
+        self.deleteitem_pushButton_2 = QtWidgets.QPushButton(self.centralwidget, clicked=lambda: self.delete_it())
         self.deleteitem_pushButton_2.setGeometry(QtCore.QRect(140, 50, 141, 31))
         self.deleteitem_pushButton_2.setObjectName("deleteitem_pushButton_2")
-        self.clearall_pushButton_3 = QtWidgets.QPushButton(self.centralwidget, clicked= lambda: self.clear_it())
+        self.clearall_pushButton_3 = QtWidgets.QPushButton(self.centralwidget, clicked=lambda: self.clear_it())
         self.clearall_pushButton_3.setGeometry(QtCore.QRect(290, 50, 101, 31))
         self.clearall_pushButton_3.setObjectName("clearall_pushButton_3")
         self.additem_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.additem_lineEdit.setGeometry(QtCore.QRect(10, 10, 381, 31))
+        self.additem_lineEdit.setGeometry(QtCore.QRect(10, 10, 501, 31))
         self.additem_lineEdit.setObjectName("additem_lineEdit")
         self.mylist_listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.mylist_listWidget.setGeometry(QtCore.QRect(10, 90, 381, 231))
+        self.mylist_listWidget.setGeometry(QtCore.QRect(10, 90, 501, 231))
         self.mylist_listWidget.setObjectName("mylist_listWidget")
+        self.savedb_pushButton = QtWidgets.QPushButton(self.centralwidget, clicked=lambda: self.save_it())
+        self.savedb_pushButton.setGeometry(QtCore.QRect(400, 50, 111, 31))
+        self.savedb_pushButton.setObjectName("savedb_pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 405, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 524, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -54,6 +57,42 @@ class Ui_MainWindow(object):
         # Delete selected row
         self.mylist_listWidget.takeItem(clicked)
 
+    # Save To Database
+    def save_it(self):
+        items = []
+        for index in range(self.mylist_listWidget.count()):
+            items.append(self.mylist_listWidget.item(index))
+
+        # Create a database or connect to one that exists
+        conn = sqlite3.connect('my_todo_list.db')
+
+        # Create a cursor instance
+        c = conn.cursor()
+
+        c.execute("""UPDATE list SET
+            item = :item,
+
+
+            WHERE oid = :oid""",
+                  {
+                      'item': fn_entry.get(),
+                      'last': ln_entry.get(),
+                      'address': address_entry.get(),
+                      'city': city_entry.get(),
+                      'state': state_entry.get(),
+                      'zipcode': zipcode_entry.get(),
+                      'oid': id_entry.get(),
+                  })
+
+        # Commit changes
+        conn.commit()
+
+        # Close our connection
+        conn.close()
+
+        for thing in items:
+            print(thing.text())
+
     # Clear All Items From List
     def clear_it(self):
         self.mylist_listWidget.clear()
@@ -64,10 +103,31 @@ class Ui_MainWindow(object):
         self.additem_pushButton.setText(_translate("MainWindow", "Add Item To List"))
         self.deleteitem_pushButton_2.setText(_translate("MainWindow", "Delete Item From List"))
         self.clearall_pushButton_3.setText(_translate("MainWindow", "Clear The List"))
+        self.savedb_pushButton.setText(_translate("MainWindow", "Save To Database"))
+
+    def thing():
+        # Create a database or connect to one that exists
+        conn = sqlite3.connect('my_todo_list.db')
+
+        # Create a cursor instance
+        c = conn.cursor()
+
+        # Create Table
+        c.execute("""CREATE TABLE if not exists todo_list (
+            item text)
+            """)
+        # Commit changes
+        conn.commit()
+
+        # Close our connection
+        conn.close()
+
+    thing()
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
